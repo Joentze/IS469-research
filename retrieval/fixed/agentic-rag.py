bb3dbf44094b0d7aa48c65a1e22451d3e1f02a25
Joen Tan <joentze01@gmail.com>
2026-03-20 11:25:27 +0800

    feat: added chunking folders

---
+++ retrieval/fixed/agentic-rag.py
@@ -0,0 +1,7 @@
+"""
+use chroma db as vector store
+https://docs.langchain.com/oss/python/integrations/vectorstores/chroma
+
+use ReAct agents with tools to search RAG database:
+https://docs.langchain.com/oss/python/langchain/agents
+"""
